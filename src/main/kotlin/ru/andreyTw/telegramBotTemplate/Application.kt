258edb6cab525feb530427b0efb4d2d2c6678b28
AndreyTW <sav_687@mail.ru>
2023-05-05 18:23:50 +0300

    telegram bot base is implemented

--- src/main/kotlin/ru/andreyTw/telegramBotTemplate/Application.kt
+++ src/main/kotlin/ru/andreyTw/telegramBotTemplate/Application.kt
@@ -1,5 +1,23 @@
 package ru.andreyTw.telegramBotTemplate
 
+import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.UpdatesListener
+import com.pengrad.telegrambot.request.SendMessage
+import java.io.File
+
 fun main() {
-    println(TextPrinter().printText())
+
+    val bot = TelegramBot(File("botKey").readText())
+    bot.setUpdatesListener { updates ->
+        updates.forEach {
+            bot.execute(SendMessage(it.message().chat().id(), TextPrinter().printText()))
+        }
+        UpdatesListener.CONFIRMED_UPDATES_ALL
+    }
+
+    while (true) {
+    }
+
+    bot.removeGetUpdatesListener()
+    bot.shutdown()
 }
